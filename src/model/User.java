8e3bda35daf21cee5459d0c8e7b57f9fa2ce94b7
zo0m.cfg <zo0m.cfg@gmail.com>
2011-05-22 16:16:41 +0000

    User update; @IKalashnikov

--- src/model/User.java
+++ src/model/User.java
@@ -1,13 +1,32 @@
 package model;
 
 public class User {
-	int id;
-	String login;
-	String password; // later md5 hash;
+	private int id;
+	private String login;
+	private String password; // later md5 hash;
 	public User() {
 		this.id = 1;
 		this.login = "admin";
 		this.password = "qwerty";
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
+	
 }
