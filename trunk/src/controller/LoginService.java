9b8a8cfc623207e3329d6e06129995210f3fff61
zo0m.cfg@gmail.com <zo0m.cfg@gmail.com@0834aaa1-921f-c1d6-ea92-3d218a959bc3>
2011-05-28 19:29:07 +0000

    Фиксы для нормального использования Hibernate на JBoss; Добавление проверки вводимого логина; @IKalashnikov

--- trunk/src/controller/LoginService.java
+++ trunk/src/controller/LoginService.java
@@ -3,6 +3,7 @@
  */
 package controller;
 
+import javax.ejb.Stateless;
 import javax.servlet.http.HttpSession;
 
 import utils.ClientDAO;
@@ -14,12 +15,19 @@ import model.Client;
  * @author Administrator
  *
  */
+@Stateless
+
 public class LoginService implements ILoginService{
 	public boolean createUserSession(String login, String password, HttpSession session) {
-		//Client u = new Client();
-		Client u = ClientDAO.getClientByLogin("boss");
-		session.setAttribute("User", u);
-		return true;
+		Client u = null;
+		u = ClientDAO.getClientByLogin(login); 
+		
+		if (u==null) {
+			return false;
+		} else {
+			session.setAttribute("User", u);
+			return true;
+		}
 	}
 	public boolean getLogin(String login,String password){
 		return true;
