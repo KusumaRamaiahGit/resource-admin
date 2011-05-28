200a16e5848e4e7b9db59a5c2abfc2ea5b8e5366
erlana_la_roshe@mail.ru <erlana_la_roshe@mail.ru@0834aaa1-921f-c1d6-ea92-3d218a959bc3>
2011-05-28 16:57:43 +0000

    add Invenarable and work with him @smihajlenko

--- trunk/src/model/Monitor.java
+++ trunk/src/model/Monitor.java
@@ -2,23 +2,52 @@ package model;
 
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+import javax.persistence.Column;
 
 /**
  * @author rsamoylov
  */
 @Entity
-@Table(name = "Monitor")
 @PrimaryKeyJoinColumn(name = "resource_id", referencedColumnName = "resource_id")
-public class Monitor extends Resource
+public class Monitor extends Resource implements Inventarable
 {
+    @Column(name = "Inventarno", unique = true, length = 10)
+    private String inventarno;
     public Monitor()
     {
         super();
     }
 
-    public Monitor(String name)
-    {
+    public Monitor(String name,String inventarno) {
         super(name);
+        this.inventarno = inventarno;
     }
+
+    public void setInvenarno(String inventarno) {
+        this.inventarno = inventarno;
+    }
+
+    public String getInvenarno() {
+        return inventarno;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        final Monitor mon = (Monitor) o;
+        if ( !inventarno.equals( mon.inventarno ) ) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode()+5*inventarno.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+" "+inventarno;
+    }
+
 }
