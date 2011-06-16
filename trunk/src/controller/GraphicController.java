fe833d1412924f0738c5985db64e96607592be57
zo0m.cfg@gmail.com <zo0m.cfg@gmail.com@0834aaa1-921f-c1d6-ea92-3d218a959bc3>
2011-06-16 09:10:27 +0000

    Resource create ; @IKalashnikov

--- trunk/src/controller/GraphicController.java
+++ trunk/src/controller/GraphicController.java
@@ -1,13 +1,14 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import utils.NextGraphic;
+import bean.NextGraphic;
 
 public class GraphicController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
--- trunk/src/utils/Main.java
+++ trunk/src/utils/Main.java
@@ -1,9 +1,18 @@
 
 package utils;
+
+import java.util.Iterator;
+import java.util.List;
+
+import model.Resource;
+ 
+
 public class Main {
     public static void main(String[] args) {
-
-  DatabaseUtil.FillDatabase();
+    	List<Resource> res = ResourceDAO.getAllResources();
+    	Iterator<Resource> it = res.iterator();
+    	System.out.println(it.next().getClass().getName());
+  //DatabaseUtil.FillDatabase();
     }
 
 }
